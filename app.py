92e9775ef337b6d0adb74e17650dd26855db9ee8
itsmeankeet <adheekariankit007@gmail.com>
2024-12-21 08:45:23 +0545

    Completed

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
+import scipy
 
 try:
     df = pd.read_csv('athlete_events.csv')
